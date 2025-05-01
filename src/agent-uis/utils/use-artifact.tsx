a85766da27be1a3a7766b232ac0e23f4e77ac9a8
Tat Dat Duong <david@duong.cz>
2025-05-01 16:54:30 +0200

    feat: useArtifact cleanup

--- src/agent-uis/utils/use-artifact.tsx
+++ src/agent-uis/utils/use-artifact.tsx
@@ -1,23 +1,24 @@
-import { Message } from "@langchain/langgraph-sdk";
-import { useStreamContext, UIMessage } from "@langchain/langgraph-sdk/react-ui";
+import { useMemo } from "react";
+import type { Message } from "@langchain/langgraph-sdk";
+import {
+  useStreamContext,
+  type UIMessage,
+} from "@langchain/langgraph-sdk/react-ui";
 
-// eslint-disable-next-line react-refresh/only-export-components
-const NoopPreview = () => null;
-
-// eslint-disable-next-line react-refresh/only-export-components
-const NoopSetOpen = () => void 0;
-
-// eslint-disable-next-line react-refresh/only-export-components
-const NoopSetContext = () => void 0;
-
-const NoopContext = {};
-
+/**
+ * Hook that obtains the artifact context provided by the `LoadExternalComponent`
+ * found in the `meta.artifact` field.
+ *
+ * @see https://github.com/langchain-ai/agent-chat-ui/blob/main/src/components/thread/messages/ai.tsx
+ */
 export function useArtifact<TContext = Record<string, unknown>>() {
   type Component = (props: {
     children: React.ReactNode;
     title?: React.ReactNode;
   }) => React.ReactNode;
+
   type Context = TContext | undefined;
+
   type Bag = {
     open: boolean;
     setOpen: (value: boolean | ((prev: boolean) => boolean)) => void;
@@ -31,14 +32,19 @@ export function useArtifact<TContext = Record<string, unknown>>() {
     { MetaType: { artifact: { content: Component } & Bag } }
   >();
 
-  return [
-    thread.meta?.artifact?.content ?? NoopPreview,
-    {
-      open: thread.meta?.artifact?.open ?? false,
-      setOpen: thread.meta?.artifact?.setOpen ?? NoopSetOpen,
+  const noop = useMemo(
+    () => ({
+      content: () => null,
 
-      context: thread.meta?.artifact?.context ?? NoopContext,
-      setContext: thread.meta?.artifact?.setContext ?? NoopSetContext,
-    },
-  ] as [Component, Bag];
+      open: false,
+      setOpen: () => void 0,
+
+      context: {},
+      setContext: () => void 0,
+    }),
+    [],
+  );
+
+  const { content, ...bag } = thread.meta?.artifact ?? noop;
+  return [content, bag] as [Component, Bag];
 }
